77f961a58e5f7832961fd9160f221cc4b906ecc6
amdade <amdadeit76@gmail.com>
2018-10-28 22:45:12 +0000

    fixed all

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
-resource "aws_instance" "instance1" {
-  ami                    = " ami-0c21ae4a3bd190229"
+resource "aws_instance" "instance2" {
+  ami                    = " ${lookup(var.amis.var.region)}"
   instance_key           = "t2.micro"
   key_name               = "project1"
   subnet_id              = "${aws_subnet.artemis-public.id}"
@@ -8,3 +8,14 @@ resource "aws_instance" "instance1" {
   user_data = "${file("apache.sh")}"
   associate_public_ip_address = "true"
 }
+
+#DB Host
+resource "aws_instance" "dbhost" {
+  ami                    = " ${lookup(var.amis.var.region)}"
+  instance_key           = "t2.micro"
+  key_name               = "project1"
+  subnet_id              = "${aws_subnet.artemis-private.id}"
+  vpc_security_group_ids = ["${aws_security_group.artemis.id}"]
+
+  user_data = "${file("db.sh")}"
+ }
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
-  value = "${aws_instance.instance1.publiic_ip}"
+  value = "${aws_instance.instance1.public_ip}"
+}
+
+output "ID" {
+  value = "${aws_instance.instance1.id}"
 }
